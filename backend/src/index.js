4e126c49e813f20e562beb63272327fb25b1f8d6
Rinav-9 <rinavchaudhari9@gmail.com>
2025-12-03 21:39:53 +0530

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
-const __dirname = path.resolve();k
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
